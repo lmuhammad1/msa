878970dfd759a6df480b1d9613a961359d763767
Louis Muhammad <louis.muhammad@gmail.com>
2016-01-11 09:53:01 -0500

    restart apache in capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,10 +46,12 @@ set :pty, true
 
 namespace :deploy do
 
-	desc "Restart Passenger app"
-task :restart do
-    run "#{ try_sudo } touch #{ File.join(current_path, 'tmp', 'restart.txt') }"
-end
+	desc 'Restart Application'
+	task :restart do
+		on roles(:app), in: :sequence, wait: 5 do
+			execute :touch, release_path.join('tmp/restart.txt')
+		end
+	end
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
